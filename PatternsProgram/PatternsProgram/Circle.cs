ee3e6fcb0dfa67703a75389ed8ef6a4e4479ff1a
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-13 09:15:46 +0300

    Add Prototype Pattern program logic

---
+++ PatternsProgram/PatternsProgram/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PatternsProgram
+{
+
+    [Serializable]
+    class Circle : IFigure
+    {
+        int radius;
+        public Point Point { get; set; }
+        public Circle(int r, int x, int y)
+        {
+            radius = r;
+            this.Point = new Point { X = x, Y = y };
+        }
+
+        public IFigure Clone()
+        {
+            return this.MemberwiseClone() as IFigure;
+        }
+
+        public object DeepCopy()
+        {
+            object figure = null;
+            using (MemoryStream tempStream = new MemoryStream())
+            {
+                BinaryFormatter binFormatter = new BinaryFormatter(null,
+                    new StreamingContext(StreamingContextStates.Clone));
+
+                binFormatter.Serialize(tempStream, this);
+                tempStream.Seek(0, SeekOrigin.Begin);
+
+                figure = binFormatter.Deserialize(tempStream);
+            }
+            return figure;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Circle with radious {0} and center in point ({1}, {2})", radius, Point.X, Point.Y);
+        }
+    }
+}
---
+++ PatternsProgram/PatternsProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle figure = new Circle(30, 50, 60);
+
+            Circle clonedFigure = figure.DeepCopy() as Circle;
+
+            figure.Point.X = 100;
+
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            Console.Read();
+        }
+
+    }
+
+
+}
